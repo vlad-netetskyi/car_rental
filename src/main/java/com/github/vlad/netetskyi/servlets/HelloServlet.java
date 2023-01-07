da913b6205fb2b211d7673f88c03eb59d027720a
Vladyslav Netetskyi <thermwaik@gmail.com>
2023-01-07 15:17:34 +0200

    added embedded tomcat and dummy servlet

---
+++ src/main/java/com/github/vlad/netetskyi/servlets/HelloServlet.java
@@ -0,0 +1,28 @@
+package com.github.vlad.netetskyi.servlets;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletOutputStream;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+@WebServlet(
+        name = "MyServlet",
+        urlPatterns = {"/hello"}
+)
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        ServletOutputStream out = resp.getOutputStream();
+        out.write("hello".getBytes());
+        out.flush();
+        out.close();
+    }
+
+}
